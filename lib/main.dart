f7f75bdc6c75bc4185de59b6d5497eb83aa79724
Priyojit <priyojitsingha01@gmail.com>
2023-08-25 19:14:14 +0530

    [!!!][FTR] Removed dynamic background

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_list_app/utils/data_storage.dart';
 import 'package:todo_list_app/utils/task_operations.dart';
 
-import 'package:todo_list_app/utils/updateBackground.dart';
+// import 'package:todo_list_app/utils/updateBackground.dart';
 import 'package:todo_list_app/widgets/appBar.dart';
 import 'package:todo_list_app/widgets/newtask.dart';
 import 'package:todo_list_app/widgets/tasklist.dart';
@@ -39,33 +39,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     loadfromlocal(_updateUI);
-    shuffleimagelinks();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // this container holds the background image
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('${imagelinks.first}'),
-          fit: BoxFit.cover,
-        ),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: appBar(),
+      body: TaskList(
+        tasklist: tasks,
+        deletetask: deletetask,
+        updatetask: updatetask,
+        updateCallback: _updateUI,
       ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: appBar(),
-        body: TaskList(
-          tasklist: tasks,
-          deletetask: deletetask,
-          updatetask: updatetask,
-          updateCallback: _updateUI,
-        ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.white54,
-            onPressed: createNewTask,
-            child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue[800],
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
       ),
     );
   }
